e5addbbc5534c45733c1d1629fd3969806e72069
wiwabzu <wiwabzu@outlook.com>
2022-10-21 20:21:03 +0800

    update

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-const
+
+
+
+
+const express = require('express')
+const https = require('node:https');
+
+const app = express()
+const port = 3000
+
+
+
+app.get('/', (req, res) => {
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=manila&units=metric&appid=bb11c58259a62ec9cd004cad3b2e6f9d";
+  https.get(url, function(response){
+    console.log(response);
+  });
+  res.send('Server is running') 
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`)
+})
